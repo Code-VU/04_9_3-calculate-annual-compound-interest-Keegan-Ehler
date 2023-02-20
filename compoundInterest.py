e6ef2aaed24d51c064c59ebc5b3803399db74a00
Keegan Ehler <Keegan.Ehler@vu.com>
2023-02-20 15:34:54 -0600

    2

--- compoundInterest.py
+++ compoundInterest.py
@@ -2,11 +2,18 @@ def calculateCompoundInterest():
     
 # This first 3 lines are provided for yougetACompoundInterest()
 # This first 3 lines are provided for you
- client_one_principal = float(input("Principle (amount): "))
- client_one_time =      float(input("Time:               "))
- client_one_rate =      float(input("Rate:               "))
- #print("Compound Interest: "+str(intrest))
-
+    for x in range(3):
+        client_one_principal = float(input("Principle (amount): "))
+        client_one_time =      float(input("Time:               "))
+        client_one_rate =      float(input("Rate:               "))
+    #print("Compound Interest: "+str(intrest))
+        A = client_one_principal * ( 1 + client_one_rate/100)**client_one_time
+        CI = A - client_one_principal
+        C = round(CI, 2)
+        print(f"Compound Interest: {C}")
+        if x<(2):
+            print("---")
+        
     # end assignment
 
 ## If you want to test locally run > python compoundInterest.py
